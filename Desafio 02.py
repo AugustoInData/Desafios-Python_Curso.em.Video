5e13642228687b321959a2e8594ddec2455fef50
Carlos Augusto <carxaug9595@gmail.com>
2024-03-16 12:29:02 -0300

    Desafio 02

---
+++ Desafio 02.py
@@ -0,0 +1,6 @@
+## Esse programa irá ler o nome do usuário,
+## e em seguida, irá responder "É um prazer te conhecer, nome do usuário"
+
+nome = input('Olá, qual o seu nome?:\n')
+
+print('É um prazer te conhecer, {}'.format(nome))
