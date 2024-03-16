3e5ea54b855a9080e19606f10ba6be5e49390a74
Carlos Augusto <carxaug9595@gmail.com>
2024-03-16 13:01:05 -0300

    Desafio 03

---
+++ Desafio 03.py
@@ -0,0 +1,9 @@
+## Esse programa irá ler dois números
+## e fará a soma entre eles
+
+num1 = int(input('Digite um número:'))
+num2 = int(input('Digite outro número:'))
+
+soma = f'A soma entre o número {num1} e o número {num2} é: {num1 + num2}'
+
+print(soma)
