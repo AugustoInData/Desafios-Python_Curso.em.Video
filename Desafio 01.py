1462a73c4e33d6e0bd38df2f397db0846e6eb5c9
Carlos Augusto <carxaug9595@gmail.com>
2024-03-16 12:26:48 -0300

    Desafio 01

---
+++ Desafio 01.py
@@ -0,0 +1,5 @@
+## Esse programa irá ler o nome do usuário
+## e em seguida irá desejar boas vindas
+
+nome_usuario = input('Qual o seu nome? \n')
+print('Boas vindas ao mundo do Python,',nome_usuario,'!\n')
