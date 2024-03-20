b4865744452d2ca19f0e1121fdf71fd608748943
Carlos Augusto <carxaug9595@gmail.com>
2024-03-20 18:54:46 -0300

    Desafio 14

---
+++ Desafio 14.py
@@ -0,0 +1,9 @@
+## Esse programa irá realizar 
+## uma conversão de temperatura entre
+## Celsius e Fahrenheit
+
+temp = float(input('Quantos ºC você deseja converter para Fahrenheit?\n'))
+
+f = (9 / 5) * temp + 32
+
+print(f'{temp}ºC convertidos em Fahrenheit são {f}ºF')
