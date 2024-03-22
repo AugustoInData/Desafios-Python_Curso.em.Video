859e53c2c4ecf01b91bfb3731586313531fd91b8
Carlos Augusto <carxaug9595@gmail.com>
2024-03-22 11:10:23 -0300

    Remover arquivo

--- Desafio 17.py
+++
@@ -1,8 +0,0 @@
-## O programa irá ler um número e seguida mostrará sua parte inteira
-
-import math
-num = float(input('Digite um número:\n'))
-
-parte_inteira = math.trunc(num)
-
-print(f' O número digitado foi {num} e sua parte inteira é {parte_inteira}')
